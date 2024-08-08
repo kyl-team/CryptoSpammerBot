5b97dcad03f5db4c9817db6391930dca56645a10
kewldan <kewldanil1@gmail.com>
2024-08-08 14:25:05 +0300

    remove unused titles

--- src/core/job/worker.py
+++ src/core/job/worker.py
@@ -281,20 +281,24 @@ async def start(user_id: int):
     for result in results:
         markdown_content += f'## Канал @{result.name} [{result.id}], беседа: {"есть" if result.has_linked_chat else "нет"}\n\n'
         markdown_content += f'Обработчик: {result.client_name}, прокси: {result.client_proxy["hostname"]}:{result.client_proxy["port"]}\n\n'
-        markdown_content += '### Ошибки\n'
-        for error in result.errors:
-            markdown_content += f' * {error}\n'
-        markdown_content += '### Участники\n'
-        i = 0
-        for member in result.members:
-            markdown_content += f'{i}. @{member.username} ({member.first_name} {member.last_name}), телефон: {member.phone}, био: "{member.bio}", найдено бесед: {len(member.chats)} [{member.id}]\n'
-            for chat in member.chats:
-                markdown_content += f'    Беседа @{chat.username}, участников: {len(chat.members)}\n'
-                j = 0
-                for chat_member in chat.members:
-                    markdown_content += f'        {j}. Участник @{chat_member.username} ({chat_member.first_name} {chat_member.last_name}), телефон: {chat_member.phone} [{chat_member.id}]\n'
-                    j += 1
-            i += 1
+
+        if len(result.errors):
+            markdown_content += '### Ошибки\n'
+            for error in result.errors:
+                markdown_content += f' * {error}\n'
+
+        if len(result.members):
+            markdown_content += '### Участники\n'
+            i = 0
+            for member in result.members:
+                markdown_content += f'{i}. @{member.username} ({member.first_name} {member.last_name}), телефон: {member.phone}, био: "{member.bio}", найдено бесед: {len(member.chats)} [{member.id}]\n'
+                for chat in member.chats:
+                    markdown_content += f'    Беседа @{chat.username}, участников: {len(chat.members)}\n'
+                    j = 0
+                    for chat_member in chat.members:
+                        markdown_content += f'        {j}. Участник @{chat_member.username} ({chat_member.first_name} {chat_member.last_name}), телефон: {chat_member.phone} [{chat_member.id}]\n'
+                        j += 1
+                i += 1
 
     report = BufferedInputFile(markdown_content.encode(),
                                filename=f"report.md")
