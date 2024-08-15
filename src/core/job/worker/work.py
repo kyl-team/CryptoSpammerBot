ece3100d1c6b503954eb33298dc896c4141f06fa
kewldan <kewldanil1@gmail.com>
2024-08-16 02:07:53 +0300

    not tests

--- src/core/job/worker/work.py
+++ src/core/job/worker/work.py
@@ -125,7 +125,6 @@ async def handle_discussion(client: Client, discussion: Chat, state: TaskState, 
 
 async def work(client: Client, channels: list[str], state: TaskState) -> WorkResult:
     results: WorkResult = []
-    a = 0
     for channel in channels:
         if state.stop_signal:
             break
@@ -156,7 +155,4 @@ async def work(client: Client, channels: list[str], state: TaskState) -> WorkRes
                         f'Не удалось зайти в чат канала. {format_exception(e)}')
 
             await handle_discussion(client, channel_chat.linked_chat, state, channel_result.linked_chat)
-        a += 1
-        if a >= 8:
-            break
     return results
